c523390ac0a065aa3f16f5603cf79fb0e0418274
oliveiramiro2 <ramiromares95@gmail.com>
2022-05-31 00:05:12 -0300

    drawing the walls in blocks

--- PacmanTrab/Display.cpp
+++ PacmanTrab/Display.cpp
@@ -26,41 +26,70 @@ Display::Display(){
 void Display::drawWall(){
     al_init_primitives_addon();
 
-    al_draw_filled_rectangle(0,DIMENSIONS_SCREEN_PACMAN,PIXEL_GAME_SIZE,0, al_map_rgb(255,0,0)); // left
-    al_draw_filled_rectangle(0,PIXEL_GAME_SIZE,DIMENSIONS_SCREEN_PACMAN,0, al_map_rgb(255,0,0)); // top
-    al_draw_filled_rectangle(DIMENSIONS_SCREEN_PACMAN,DIMENSIONS_SCREEN_PACMAN-PIXEL_GAME_SIZE,0,DIMENSIONS_SCREEN_PACMAN, al_map_rgb(255,0,0)); // bottom
-    al_draw_filled_rectangle(DIMENSIONS_SCREEN_PACMAN-PIXEL_GAME_SIZE,0,DIMENSIONS_SCREEN_PACMAN,DIMENSIONS_SCREEN_PACMAN, al_map_rgb(255,0,0)); // right
-
-    al_draw_filled_rectangle(2*PIXEL_GAME_SIZE, 2*PIXEL_GAME_SIZE, 3*PIXEL_GAME_SIZE, 9*PIXEL_GAME_SIZE, al_map_rgb(255,0,0));
-    al_draw_filled_rectangle((MAX_SIZE_TABLE-2)*PIXEL_GAME_SIZE, 2*PIXEL_GAME_SIZE, (MAX_SIZE_TABLE-3)*PIXEL_GAME_SIZE, 9*PIXEL_GAME_SIZE, al_map_rgb(255,0,0));
-
-    al_draw_filled_rectangle(2*PIXEL_GAME_SIZE, (MAX_SIZE_TABLE-2)*PIXEL_GAME_SIZE, 3*PIXEL_GAME_SIZE, (MAX_SIZE_TABLE-9)*PIXEL_GAME_SIZE, al_map_rgb(255,0,0));
-    al_draw_filled_rectangle((MAX_SIZE_TABLE-2)*PIXEL_GAME_SIZE, (MAX_SIZE_TABLE-2)*PIXEL_GAME_SIZE, (MAX_SIZE_TABLE-3)*PIXEL_GAME_SIZE, (MAX_SIZE_TABLE-9)*PIXEL_GAME_SIZE, al_map_rgb(255,0,0));
+    // wall around
+    for(int i=1; i<MAX_SIZE_TABLE; i++){
+        al_draw_filled_rectangle(0, ((MAX_SIZE_TABLE-i)*PIXEL_GAME_SIZE)-1, PIXEL_GAME_SIZE, (MAX_SIZE_TABLE-i-1)*(PIXEL_GAME_SIZE), al_map_rgb(255,0,0)); // left
+        al_draw_filled_rectangle((MAX_SIZE_TABLE-i+1)*(PIXEL_GAME_SIZE),PIXEL_GAME_SIZE,((MAX_SIZE_TABLE-i+2)*PIXEL_GAME_SIZE)-1, 0, al_map_rgb(255,0,0)); // top
+        al_draw_filled_rectangle(((MAX_SIZE_TABLE-i)*PIXEL_GAME_SIZE)-1,DIMENSIONS_SCREEN_PACMAN-PIXEL_GAME_SIZE,(MAX_SIZE_TABLE-i-1)*(PIXEL_GAME_SIZE),DIMENSIONS_SCREEN_PACMAN, al_map_rgb(255,0,0)); // bottom
+        al_draw_filled_rectangle(DIMENSIONS_SCREEN_PACMAN-PIXEL_GAME_SIZE,(i * PIXEL_GAME_SIZE)+1, DIMENSIONS_SCREEN_PACMAN,((i+1)*PIXEL_GAME_SIZE)-1, al_map_rgb(255,0,0)); // right
+    }
+    al_draw_filled_rectangle(36,PIXEL_GAME_SIZE,69, 0, al_map_rgb(255,0,0));
 
 
-    al_draw_filled_rectangle(4*PIXEL_GAME_SIZE, 2*PIXEL_GAME_SIZE, 5*PIXEL_GAME_SIZE, 6*PIXEL_GAME_SIZE, al_map_rgb(255,0,0));
-    al_draw_filled_rectangle(6*PIXEL_GAME_SIZE, 2*PIXEL_GAME_SIZE, 7*PIXEL_GAME_SIZE, 6*PIXEL_GAME_SIZE, al_map_rgb(255,0,0));
-    al_draw_filled_rectangle(8*PIXEL_GAME_SIZE, 2*PIXEL_GAME_SIZE, 9*PIXEL_GAME_SIZE, 6*PIXEL_GAME_SIZE, al_map_rgb(255,0,0));
-    al_draw_filled_rectangle(10*PIXEL_GAME_SIZE, 2*PIXEL_GAME_SIZE, 11*PIXEL_GAME_SIZE, 6*PIXEL_GAME_SIZE, al_map_rgb(255,0,0));
-    al_draw_filled_rectangle(12*PIXEL_GAME_SIZE, 2*PIXEL_GAME_SIZE, 13*PIXEL_GAME_SIZE, 6*PIXEL_GAME_SIZE, al_map_rgb(255,0,0));
-    al_draw_filled_rectangle(14*PIXEL_GAME_SIZE, 2*PIXEL_GAME_SIZE, 15*PIXEL_GAME_SIZE, 6*PIXEL_GAME_SIZE, al_map_rgb(255,0,0));
+    // Wall inside
+    for(int i=1; i<MAX_SIZE_TABLE; i++){
+        // upside horizontal
+        if(i >= 2 && i <= 5){
+            al_draw_filled_rectangle(4*PIXEL_GAME_SIZE, i*PIXEL_GAME_SIZE, 5*PIXEL_GAME_SIZE, (((i+1)*PIXEL_GAME_SIZE)-1), al_map_rgb(255,0,0));
+            al_draw_filled_rectangle(6*PIXEL_GAME_SIZE, i*PIXEL_GAME_SIZE, 7*PIXEL_GAME_SIZE, (((i+1)*PIXEL_GAME_SIZE)-1), al_map_rgb(255,0,0));
+            al_draw_filled_rectangle(8*PIXEL_GAME_SIZE, i*PIXEL_GAME_SIZE, 9*PIXEL_GAME_SIZE, (((i+1)*PIXEL_GAME_SIZE)-1), al_map_rgb(255,0,0));
+            al_draw_filled_rectangle(10*PIXEL_GAME_SIZE, i*PIXEL_GAME_SIZE, 11*PIXEL_GAME_SIZE, (((i+1)*PIXEL_GAME_SIZE)-1), al_map_rgb(255,0,0));
+            al_draw_filled_rectangle(12*PIXEL_GAME_SIZE, i*PIXEL_GAME_SIZE, 13*PIXEL_GAME_SIZE, (((i+1)*PIXEL_GAME_SIZE)-1), al_map_rgb(255,0,0));
+            al_draw_filled_rectangle(14*PIXEL_GAME_SIZE, i*PIXEL_GAME_SIZE, 15*PIXEL_GAME_SIZE, (((i+1)*PIXEL_GAME_SIZE)-1), al_map_rgb(255,0,0));
+        }
 
+        // downside horizontal
+        if(i >= 2 && i <= 6){
+                if(i <= 5){
+                    al_draw_filled_rectangle((MAX_SIZE_TABLE-6)*PIXEL_GAME_SIZE, (MAX_SIZE_TABLE-i)*PIXEL_GAME_SIZE, (MAX_SIZE_TABLE-7)*PIXEL_GAME_SIZE, (((MAX_SIZE_TABLE-(i+1))*PIXEL_GAME_SIZE)+1), al_map_rgb(255,0,0));
+                    al_draw_filled_rectangle((MAX_SIZE_TABLE-10)*PIXEL_GAME_SIZE, (MAX_SIZE_TABLE-i)*PIXEL_GAME_SIZE, (MAX_SIZE_TABLE-11)*PIXEL_GAME_SIZE, (((MAX_SIZE_TABLE-(i+1))*PIXEL_GAME_SIZE)+1), al_map_rgb(255,0,0));
+                    al_draw_filled_rectangle((MAX_SIZE_TABLE-14)*PIXEL_GAME_SIZE, (MAX_SIZE_TABLE-i)*PIXEL_GAME_SIZE, (MAX_SIZE_TABLE-15)*PIXEL_GAME_SIZE, (((MAX_SIZE_TABLE-(i+1))*PIXEL_GAME_SIZE)+1), al_map_rgb(255,0,0));
+                }
+                if(i >= 2 && i <= 7){
+                    al_draw_filled_rectangle((MAX_SIZE_TABLE-4)*PIXEL_GAME_SIZE, (MAX_SIZE_TABLE-i)*PIXEL_GAME_SIZE, (MAX_SIZE_TABLE-5)*PIXEL_GAME_SIZE, (((MAX_SIZE_TABLE-(i+1))*PIXEL_GAME_SIZE)+1), al_map_rgb(255,0,0));
+                    al_draw_filled_rectangle((MAX_SIZE_TABLE-8)*PIXEL_GAME_SIZE, (MAX_SIZE_TABLE-i)*PIXEL_GAME_SIZE, (MAX_SIZE_TABLE-9)*PIXEL_GAME_SIZE, (((MAX_SIZE_TABLE-(i+1))*PIXEL_GAME_SIZE)+1), al_map_rgb(255,0,0));
+                    al_draw_filled_rectangle((MAX_SIZE_TABLE-12)*PIXEL_GAME_SIZE, (MAX_SIZE_TABLE-i)*PIXEL_GAME_SIZE, (MAX_SIZE_TABLE-13)*PIXEL_GAME_SIZE, (((MAX_SIZE_TABLE-(i+1))*PIXEL_GAME_SIZE)+1), al_map_rgb(255,0,0));
+                }
+                al_draw_filled_rectangle((MAX_SIZE_TABLE-6)*PIXEL_GAME_SIZE, (((MAX_SIZE_TABLE-1)*PIXEL_GAME_SIZE)-1), (MAX_SIZE_TABLE-7)*PIXEL_GAME_SIZE, (((MAX_SIZE_TABLE-(2))*PIXEL_GAME_SIZE)+1), al_map_rgb(255,0,0));
+                    al_draw_filled_rectangle((MAX_SIZE_TABLE-10)*PIXEL_GAME_SIZE, (((MAX_SIZE_TABLE-1)*PIXEL_GAME_SIZE)-1), (MAX_SIZE_TABLE-11)*PIXEL_GAME_SIZE, (((MAX_SIZE_TABLE-(2))*PIXEL_GAME_SIZE)+1), al_map_rgb(255,0,0));
+                    al_draw_filled_rectangle((MAX_SIZE_TABLE-14)*PIXEL_GAME_SIZE, (((MAX_SIZE_TABLE-1)*PIXEL_GAME_SIZE)-1), (MAX_SIZE_TABLE-15)*PIXEL_GAME_SIZE, (((MAX_SIZE_TABLE-(2))*PIXEL_GAME_SIZE)+1), al_map_rgb(255,0,0));
+        }
 
-    al_draw_filled_rectangle((MAX_SIZE_TABLE-4)*PIXEL_GAME_SIZE, (MAX_SIZE_TABLE-2)*PIXEL_GAME_SIZE, (MAX_SIZE_TABLE-5)*PIXEL_GAME_SIZE, (MAX_SIZE_TABLE-7)*PIXEL_GAME_SIZE, al_map_rgb(255,0,0));
-    al_draw_filled_rectangle((MAX_SIZE_TABLE-6)*PIXEL_GAME_SIZE, (MAX_SIZE_TABLE-1)*PIXEL_GAME_SIZE, (MAX_SIZE_TABLE-7)*PIXEL_GAME_SIZE, (MAX_SIZE_TABLE-6)*PIXEL_GAME_SIZE, al_map_rgb(255,0,0));
-    al_draw_filled_rectangle((MAX_SIZE_TABLE-8)*PIXEL_GAME_SIZE, (MAX_SIZE_TABLE-2)*PIXEL_GAME_SIZE, (MAX_SIZE_TABLE-9)*PIXEL_GAME_SIZE, (MAX_SIZE_TABLE-7)*PIXEL_GAME_SIZE, al_map_rgb(255,0,0));
-    al_draw_filled_rectangle((MAX_SIZE_TABLE-10)*PIXEL_GAME_SIZE, (MAX_SIZE_TABLE-1)*PIXEL_GAME_SIZE, (MAX_SIZE_TABLE-11)*PIXEL_GAME_SIZE, (MAX_SIZE_TABLE-6)*PIXEL_GAME_SIZE, al_map_rgb(255,0,0));
-    al_draw_filled_rectangle((MAX_SIZE_TABLE-12)*PIXEL_GAME_SIZE, (MAX_SIZE_TABLE-2)*PIXEL_GAME_SIZE, (MAX_SIZE_TABLE-13)*PIXEL_GAME_SIZE, (MAX_SIZE_TABLE-7)*PIXEL_GAME_SIZE, al_map_rgb(255,0,0));
-    al_draw_filled_rectangle((MAX_SIZE_TABLE-14)*PIXEL_GAME_SIZE, (MAX_SIZE_TABLE-1)*PIXEL_GAME_SIZE, (MAX_SIZE_TABLE-15)*PIXEL_GAME_SIZE, (MAX_SIZE_TABLE-6)*PIXEL_GAME_SIZE, al_map_rgb(255,0,0));
+        // bigger column horizontal upside and downside
+        if(i >= 2 && i <= 8 ){
+            al_draw_filled_rectangle(2*PIXEL_GAME_SIZE, i*PIXEL_GAME_SIZE, 3*PIXEL_GAME_SIZE, (((i+1)*PIXEL_GAME_SIZE)-1), al_map_rgb(255,0,0));
+            al_draw_filled_rectangle((MAX_SIZE_TABLE-2)*PIXEL_GAME_SIZE, i*PIXEL_GAME_SIZE, (MAX_SIZE_TABLE-3)*PIXEL_GAME_SIZE, (((i+1)*PIXEL_GAME_SIZE)-1), al_map_rgb(255,0,0));
 
-    al_draw_filled_rectangle(4*PIXEL_GAME_SIZE, 7*PIXEL_GAME_SIZE, 9*PIXEL_GAME_SIZE, 8*PIXEL_GAME_SIZE, al_map_rgb(255,0,0));
-    al_draw_filled_rectangle(10*PIXEL_GAME_SIZE, 7*PIXEL_GAME_SIZE, 15*PIXEL_GAME_SIZE, 8*PIXEL_GAME_SIZE, al_map_rgb(255,0,0));
+            al_draw_filled_rectangle(2*PIXEL_GAME_SIZE, (MAX_SIZE_TABLE-i)*PIXEL_GAME_SIZE, 3*PIXEL_GAME_SIZE, (((MAX_SIZE_TABLE-(i+1))*PIXEL_GAME_SIZE)+1), al_map_rgb(255,0,0));
+            al_draw_filled_rectangle((MAX_SIZE_TABLE-2)*PIXEL_GAME_SIZE, (MAX_SIZE_TABLE-i)*PIXEL_GAME_SIZE, (MAX_SIZE_TABLE-3)*PIXEL_GAME_SIZE, (((MAX_SIZE_TABLE-(i+1))*PIXEL_GAME_SIZE)+1), al_map_rgb(255,0,0));
+        }
 
-    al_draw_filled_rectangle(4*PIXEL_GAME_SIZE, (MAX_SIZE_TABLE-7)*PIXEL_GAME_SIZE, 9*PIXEL_GAME_SIZE, (MAX_SIZE_TABLE-8)*PIXEL_GAME_SIZE, al_map_rgb(255,0,0));
-    al_draw_filled_rectangle(10*PIXEL_GAME_SIZE, (MAX_SIZE_TABLE-7)*PIXEL_GAME_SIZE, 15*PIXEL_GAME_SIZE, (MAX_SIZE_TABLE-8)*PIXEL_GAME_SIZE, al_map_rgb(255,0,0));
+        // vertical walls
+        if(i >= 4 && i <= 15){
+            if(i >= 4 && i <= 8){
+                al_draw_filled_rectangle(i*PIXEL_GAME_SIZE, 7*PIXEL_GAME_SIZE, (((i+1)*PIXEL_GAME_SIZE)-1), 8*PIXEL_GAME_SIZE, al_map_rgb(255,0,0));
+                al_draw_filled_rectangle(i*PIXEL_GAME_SIZE, (MAX_SIZE_TABLE-7)*PIXEL_GAME_SIZE, (((i+1)*PIXEL_GAME_SIZE)-1), (MAX_SIZE_TABLE-8)*PIXEL_GAME_SIZE, al_map_rgb(255,0,0));
+            }
 
+            if(i >= 10 && i <= 14){
+                al_draw_filled_rectangle(i*PIXEL_GAME_SIZE, 7*PIXEL_GAME_SIZE, (((i+1)*PIXEL_GAME_SIZE)-1), 8*PIXEL_GAME_SIZE, al_map_rgb(255,0,0));
+                al_draw_filled_rectangle(i*PIXEL_GAME_SIZE, (MAX_SIZE_TABLE-7)*PIXEL_GAME_SIZE, (((i+1)*PIXEL_GAME_SIZE)-1), (MAX_SIZE_TABLE-8)*PIXEL_GAME_SIZE, al_map_rgb(255,0,0));
+            }
 
-    al_draw_filled_rectangle(4*PIXEL_GAME_SIZE, 9*PIXEL_GAME_SIZE, 15*PIXEL_GAME_SIZE, 10*PIXEL_GAME_SIZE, al_map_rgb(255,0,0));
+            if(i >= 4 && i <= 14)
+                al_draw_filled_rectangle(i*PIXEL_GAME_SIZE, 9*PIXEL_GAME_SIZE, (((i+1)*PIXEL_GAME_SIZE)-1), 10*PIXEL_GAME_SIZE, al_map_rgb(255,0,0));
+        }
+    }
 
 }
 
