d5abfb3d5f4dcbb3c54298261d40d0de27ec234d
oliveiramiro2 <ramiromares95@gmail.com>
2022-07-22 00:40:30 -0300

    adding comments inside code to better understanding and remove some old codes

--- PacmanTrab/Ghost.cpp
+++ PacmanTrab/Ghost.cpp
@@ -8,9 +8,9 @@
 using namespace std;
 
 Ghost::Ghost(int type){
-    srand(time(NULL));
+    srand(time(NULL));                                                                      // plantando uma semente para gerar numeros aleatorios
 
-    switch(type){
+    switch(type){                                                                           // escolhendo o tipo de fantasma e colocando na posicao inicial
     case 1:
         Ghost::setPosX(PIXEL_GAME_SIZE);
         Ghost::setPosY(PIXEL_GAME_SIZE);
@@ -39,32 +39,32 @@ Ghost::Ghost(int type){
 }
 
 Ghost::~Ghost(){
-    al_destroy_bitmap(this->ghost);
+    al_destroy_bitmap(this->ghost);                                                                         // desalocando ponteiro do bitman dos fantasmas
 }
 
 
-void Ghost::changeDirection(int table[19][19], int x, int y, int pacmanX, int pacmanY){
-    if(this->getTypeStalker() % 2 == 0){
+void Ghost::changeDirection(int table[19][19], int x, int y, int pacmanX, int pacmanY){                     // mudando a direcao do fantasma
+    if(this->getTypeStalker() % 2 == 0){                                                                    // fantasmas 2 e 4 serao aleatorios
         this->setPositionMove((rand() % 4) + 1);
         return;
     }
 
-    if((y == pacmanY) && ((x > pacmanX) && (table[y][x-1] != 0))){
+    if((y == pacmanY) && (x > pacmanX) && (table[y][x-1] != 0)){                                            // checa se esta no msm sqm no y e se é possivel diminiur a distancia do x
         this->setPositionMove(4);
         return;
     }
 
-    if((y == pacmanY) && ((x < pacmanX) && (table[y][x+1] != 0))){
+    if((y == pacmanY) && (x < pacmanX) && (table[y][x+1] != 0)){                                            // checa se esta no msm sqm no y e se é possivel diminiur a distancia do x
         this->setPositionMove(2);
         return;
     }
 
-    if((x == pacmanX) && ((y > pacmanY) && (table[y-1][x] != 0))){
+    if((x == pacmanX) && (y > pacmanY) && (table[y-1][x] != 0)){                                            // checa se esta no msm sqm no x e se é possivel diminiur a distancia do y
         this->setPositionMove(1);
         return;
     }
 
-    if((x == pacmanX) && ((y < pacmanY) && (table[y+1][x] != 0))){
+    if((x == pacmanX) && (y < pacmanY) && (table[y+1][x] != 0)){                                            // checa se esta no msm sqm no x e se é possivel diminiur a distancia do y
         this->setPositionMove(3);
         return;
     }
@@ -72,174 +72,37 @@ void Ghost::changeDirection(int table[19][19], int x, int y, int pacmanX, int pa
 
 
 
-    if((x > pacmanX) && (table[y][x-1] != 0)){
+    if((x > pacmanX) && (table[y][x-1] != 0)){                                                              // checa se o fantasma esta numa posicao maior no eixo x se e possivel diminuir
         this->setPositionMove(4);
         return;
     }
 
-    if((x < pacmanX) && (table[y][x+1] != 0)){
+    if((x < pacmanX) && (table[y][x+1] != 0)){                                                              // checa se o fantasma esta numa posicao menor no eixo x se e possivel aumentar
         this->setPositionMove(2);
         return;
     }
 
-    if((y > pacmanY) && (table[y-1][x] != 0)){
+    if((y > pacmanY) && (table[y-1][x] != 0)){                                                              // checa se o fantasma esta numa posicao maior no eixo y se e possivel diminuir
         this->setPositionMove(1);
         return;
     }
 
-    if((y < pacmanY) && (table[y+1][x] != 0)){
+    if((y < pacmanY) && (table[y+1][x] != 0)){                                                              // checa se o fantasma esta numa posicao menor no eixo y se e possivel aumentar
         this->setPositionMove(3);
         return;
     }
 
-    // cout << "ValorX: \n" << pacmanX << "    -   ValorY: " << pacmanY << endl;
-    /*int biggerX, biggerY;
-    bool xIsBigger, yIsBigger;
-
-    if(x > pacmanX){
-        biggerX = x - pacmanX;
-        xIsBigger = true;
-    }else{
-        biggerX = pacmanX - x;
-        xIsBigger = false;
-    }if(y > pacmanX){
-        biggerY = y - pacmanX;
-        yIsBigger = true;
-    }else{
-        biggerY = pacmanX - y;
-        yIsBigger = false;
-    }
-
-    if(biggerX == 0){
-        if(y < pacmanY){
-            if(table[(this->getPosY() / PIXEL_GAME_SIZE)+1][this->getPosX() / PIXEL_GAME_SIZE] != 0 && this->getPosX() > 34 && this->getPosY() > 34){
-                this->setPositionMove(3);
-                return;
-            }
-        }else{
-            if(table[(this->getPosY() / PIXEL_GAME_SIZE)-1][this->getPosX() / PIXEL_GAME_SIZE] != 0 && this->getPosX() > 34 && this->getPosY() > 34){
-               this->setPositionMove(1);
-                return;
-            }
-        }
-    }
-
-    if(biggerY == 0){
-        if(x < pacmanX){
-            if(table[this->getPosY() / PIXEL_GAME_SIZE][(this->getPosX() / PIXEL_GAME_SIZE)+1] != 0 && this->getPosX() > 34 && this->getPosY() > 34){
-                this->setPositionMove(2);
-                return;
-            }
-        }else{
-            if(table[this->getPosY() / PIXEL_GAME_SIZE][this->getPosX() / PIXEL_GAME_SIZE-1] != 0 && this->getPosX() > 34 && this->getPosY() > 34){
-                this->setPositionMove(4);
-                return;
-            }
-        }
-    }
-
-    if(biggerX < biggerY){
-        if(xIsBigger && biggerY < 8){
-            if(table[this->getPosY() / PIXEL_GAME_SIZE][this->getPosX() / PIXEL_GAME_SIZE-1] != 0 && this->getPosX() > 34 && this->getPosY() > 34){
-                this->setPositionMove(4);
-                return;
-            }else if(yIsBigger){
-                if(table[(this->getPosY() / PIXEL_GAME_SIZE)-1][this->getPosX() / PIXEL_GAME_SIZE] != 0 && this->getPosX() > 34 && this->getPosY() > 34){
-                   this->setPositionMove(1);
-                    return;
-                }
-            }else if(!yIsBigger){
-                if(table[(this->getPosY() / PIXEL_GAME_SIZE)+1][this->getPosX() / PIXEL_GAME_SIZE] != 0 && this->getPosX() > 34 && this->getPosY() > 34){
-                    this->setPositionMove(3);
-                    return;
-                }
-            }
-        }else if(!xIsBigger && biggerY < 8){
-            if(table[this->getPosY() / PIXEL_GAME_SIZE][(this->getPosX() / PIXEL_GAME_SIZE)+1] != 0 && this->getPosX() > 34 && this->getPosY() > 34){
-                this->setPositionMove(2);
-                return;
-            }else if(yIsBigger){
-                if(table[(this->getPosY() / PIXEL_GAME_SIZE)-1][this->getPosX() / PIXEL_GAME_SIZE] != 0 && this->getPosX() > 34 && this->getPosY() > 34){
-                   this->setPositionMove(1);
-                    return;
-                }
-            }else if(!yIsBigger){
-                if(table[(this->getPosY() / PIXEL_GAME_SIZE)+1][this->getPosX() / PIXEL_GAME_SIZE] != 0 && this->getPosX() > 34 && this->getPosY() > 34){
-                    this->setPositionMove(3);
-                    return;
-                }
-            }
-        }else if(biggerY > 8){
-            if(yIsBigger){
-                if(table[(this->getPosY() / PIXEL_GAME_SIZE)-1][this->getPosX() / PIXEL_GAME_SIZE] != 0 && this->getPosX() > 34 && this->getPosY() > 34){
-                   this->setPositionMove(1);
-                    return;
-                }
-            }else if(!yIsBigger){
-                if(table[(this->getPosY() / PIXEL_GAME_SIZE)+1][this->getPosX() / PIXEL_GAME_SIZE] != 0 && this->getPosX() > 34 && this->getPosY() > 34){
-                    this->setPositionMove(3);
-                    return;
-                }
-            }
-        }
-    }else{
-        if(yIsBigger && biggerX < 8){
-            if(table[(this->getPosY() / PIXEL_GAME_SIZE)-1][this->getPosX() / PIXEL_GAME_SIZE] != 0 && this->getPosX() > 34 && this->getPosY() > 34){
-                this->setPositionMove(1);
-                return;
-            }else if(xIsBigger){
-                if(table[this->getPosY() / PIXEL_GAME_SIZE][this->getPosX() / PIXEL_GAME_SIZE-1] != 0 && this->getPosX() > 34 && this->getPosY() > 34){
-                    this->setPositionMove(4);
-                    return;
-                }
-            }else if(!xIsBigger){
-                if(table[this->getPosY() / PIXEL_GAME_SIZE][(this->getPosX() / PIXEL_GAME_SIZE)+1] != 0 && this->getPosX() > 34 && this->getPosY() > 34){
-                    this->setPositionMove(2);
-                    return;
-                }
-            }
-        }else if(!yIsBigger && biggerX < 8){
-            if(table[(this->getPosY() / PIXEL_GAME_SIZE)+1][this->getPosX() / PIXEL_GAME_SIZE] != 0 && this->getPosX() > 34 && this->getPosY() > 34){
-                this->setPositionMove(3);
-                return;
-            }else if(xIsBigger){
-                if(table[this->getPosY() / PIXEL_GAME_SIZE][this->getPosX() / PIXEL_GAME_SIZE-1] != 0 && this->getPosX() > 34 && this->getPosY() > 34){
-                    this->setPositionMove(4);
-                    return;
-                }
-            }else if(!xIsBigger){
-                if(table[this->getPosY() / PIXEL_GAME_SIZE][(this->getPosX() / PIXEL_GAME_SIZE)+1] != 0 && this->getPosX() > 34 && this->getPosY() > 34){
-                    this->setPositionMove(2);
-                    return;
-                }
-            }
-        }else if(biggerX > 8){
-            if(xIsBigger){
-                if(table[this->getPosY() / PIXEL_GAME_SIZE][this->getPosX() / PIXEL_GAME_SIZE-1] != 0 && this->getPosX() > 34 && this->getPosY() > 34){
-                    this->setPositionMove(4);
-                    return;
-                }
-            }else if(!xIsBigger){
-                if(table[this->getPosY() / PIXEL_GAME_SIZE][(this->getPosX() / PIXEL_GAME_SIZE)+1] != 0 && this->getPosX() > 34 && this->getPosY() > 34){
-                    this->setPositionMove(2);
-                    return;
-                }
-            }
-        }
-    }*/
-
-    this->setPositionMove((rand() % 4) + 1);
-    cout << "FOI RANDOM fdhsjkfhds\n" << endl;
+    this->setPositionMove((rand() % 4) + 1);                                                                // caso tudo der errado a posicao sera aleatoria
     return;
 }
 
-void Ghost::checkSide(int table[19][19], int x, int y, int pacmanX, int pacmanY){
-    if(this->getPositionMove() == 1 || this->getPositionMove() == 3){
-        if(table[y][x+1] != 0 || table[y][x-1] != 0){
+void Ghost::checkSide(int table[19][19], int x, int y, int pacmanX, int pacmanY){                           // checa os lados em uma encruzilhada
+    if(this->getPositionMove() == 1 || this->getPositionMove() == 3){                                       // movendo no eixo y?
+        if(table[y][x+1] != 0 || table[y][x-1] != 0){                                                       // tem posicoes livres no eixo x?
             bool lock = true;
 
-            while(lock){
-                this->changeDirection(table, x, y, pacmanX, pacmanY);
+            while(lock){                                                                                    // laco p/ setar valor ate sair um valido
+                this->changeDirection(table, x, y, pacmanX, pacmanY);                                       // setando a direcao e verificando a disponibilidade
                 if(this->getPositionMove() == 1){
                     if(table[(this->getPosY() / PIXEL_GAME_SIZE)-1][this->getPosX() / PIXEL_GAME_SIZE] != 0 && this->getPosX() > 34 && this->getPosY() > 34)
                         lock = false;
@@ -256,13 +119,13 @@ void Ghost::checkSide(int table[19][19], int x, int y, int pacmanX, int pacmanY)
             }
 
         }
-    }else if(this->getPositionMove() == 2 || this->getPositionMove() == 4){
+    }else if(this->getPositionMove() == 2 || this->getPositionMove() == 4){                                 // movendo no eixo x?
 
-        if(table[y+1][x] != 0 || table[y-1][x] != 0){
+        if(table[y+1][x] != 0 || table[y-1][x] != 0){                                                       // tem espaco disponiveis no eixo y?
             bool lock = true;
 
-            while(lock){
-                this->changeDirection(table, x, y, pacmanX, pacmanY);
+            while(lock){                                                                                    // laco p/ setar valor ate sair um valido
+                this->changeDirection(table, x, y, pacmanX, pacmanY);                                       // setando a direcao e verificando a disponibilidade
                 if(this->getPositionMove() == 1){
                     if(table[(this->getPosY() / PIXEL_GAME_SIZE)-1][this->getPosX() / PIXEL_GAME_SIZE] != 0 && this->getPosX() > 34 && this->getPosY() > 34)
                         lock = false;
